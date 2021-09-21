5275ab0bc5fe920370d2296ee99404a0ba967081
Per Olav Eide Svendsen <peesv@equinor.com>
2021-09-21 18:39:57 +0200

    Revert "rename smry to timeseries"
    This reverts commit a038332b23134d4c877babde333365c3be2f4ed0.

--- tests/test_export_item.py
+++ tests/test_export_item.py
@@ -302,22 +302,7 @@ def test_data_process_content_fluid_contact():
         exportitem._data_process_content()
     assert "is not valid for" in str(errmsg)
 
-def test_data_process_content_field_outline():
-    """Test the field field_outline."""
-
-    # test case 1
-    dataio = fmu.dataio.ExportData(
-        name="Valysar",
-        config=CFG2,
-        content={"field_outline": {"contact": "owc"}},
-        timedata=[["20210101", "first"], [20210902, "second"]],
-        tagname="WhatEver",
-    )
-    obj = xtgeo.Polygons()
-    exportitem = ei._ExportItem(dataio, obj, verbosity="INFO")
-    exportitem._data_process_content()
-
-    # test case 2
+    # test case 3
     dataio = fmu.dataio.ExportData(
         name="Valysar",
         config=CFG2,
@@ -326,24 +311,7 @@ def test_data_process_content_field_outline():
         tagname="WhatEver",
     )
     obj = xtgeo.Polygons()
-    with pytest.raises(ei.ValidationError) as errmsg:
-        exportitem = ei._ExportItem(dataio, obj, verbosity="INFO")
-        exportitem._data_process_content()
-    assert "is not valid for" in str(errmsg)
 
-    # test case 3
-    dataio = fmu.dataio.ExportData(
-        name="Valysar",
-        config=CFG2,
-        content="field_outline",
-        timedata=[["20210101", "first"], [20210902, "second"]],
-        tagname="WhatEver",
-    )
-    obj = xtgeo.Polygons()
-    with pytest.raises(ei.ValidationError) as errmsg:
-        exportitem = ei._ExportItem(dataio, obj, verbosity="INFO")
-        exportitem._data_process_content()
-    assert "requires additional input" in str(errmsg)
 
 def test_display():
     """
